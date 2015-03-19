65de6213cccd99d0625a662757002f2107f2039f
Matthew Peveler <matt.peveler@gmail.com>
2015-03-19 10:51:14 -0400

    fix broken tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,14 +1,14 @@
 # pylint: disable=missing-docstring,no-value-for-parameter
 import os
 import pylint_runner.main as runner
-from nose.tools import assert_raises, assert_equal, assert_list_equal
+from nose.tools import assert_raises, assert_equal, assert_items_equal
 
 
 def test_get_files():
     cur_dir = os.path.dirname(os.path.realpath(__file__))
     actual = runner.get_files_from_dir(cur_dir)
     expected = [cur_dir+'/__init__.py', cur_dir+'/test_runner.py']
-    assert_list_equal(expected, actual)
+    assert_items_equal(expected, actual)
 
 
 def test_runner():
